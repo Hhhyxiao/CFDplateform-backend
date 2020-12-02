c786570c3007554390175dbf9985be69430fce4a
hanyuxiao <hanyuxiao.hyx@bytedance.com>
2020-12-03 01:49:49 +0800

    1

--- src/main/java/hyx/plateform/controller/UploadFileController.java
+++ src/main/java/hyx/plateform/controller/UploadFileController.java
@@ -1,10 +1,13 @@
 package hyx.plateform.controller;
 
+import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author hanyuxiao
@@ -15,7 +18,7 @@ import org.springframework.web.multipart.MultipartFile;
 public class UploadFileController {
 
     @PostMapping("/**")
-    public void upLoadFile(@RequestParam("file") MultipartFile uploadFiles){
+    public void upLoadFile(@RequestParam("file")MultipartFile uploadFiles){
         System.out.println(uploadFiles);
     }
 }
